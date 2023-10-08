640296f3a6399f244edcb60089f968a42167a9bf
AsselK2023 <122744321+AsselK2023@users.noreply.github.com>
2023-10-08 13:36:16 +0600

    assign 2

--- main.py
+++ main.py
@@ -1,71 +1,76 @@
+# device.py
+class Device:
+    def __init__(self, name: str):
+        self.name = name
+        self.state = "off"
+
+    def turn_on(self):
+        self.state = "on"
+
+    def turn_off(self):
+        self.state = "off"
+
+# strategy.py
+from abc import ABC, abstractmethod
+
+class Strategy(ABC):
+    @abstractmethod
+    def execute(self, device):
+        pass
+
+class HeatingStrategy(Strategy):
+    def execute(self, device):
+        if "heating" in device.name.lower():
+            device.turn_on()
+            print(f"Turning on heating for {device.name}")
+
+class LightingStrategy(Strategy):
+    def execute(self, device):
+        if "light" in device.name.lower():
+            device.turn_on()
+            print(f"Turning on lights for {device.name}")
+
+# controller.py
+from typing import List
+
 class HomeController:
-    _instance = None
+    def __init__(self):
+        self.devices: List[Device] = []
+        self.strategy: Strategy = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.init_controller()
-        return cls._instance
-
-    def init_controller(self):
-        self.devices = []
-        self.strategy = None
-
-    def run(self):
-        if self.strategy:
-            self.strategy.execute(self.devices)
-
-    def add_device(self, device):
+    def add_device(self, device: Device):
         self.devices.append(device)
 
-    def set_strategy(self, strategy):
+    def set_strategy(self, strategy: Strategy):
         self.strategy = strategy
 
-    def control_device(self, device_name):
+    def control_device(self, device_name: str):
+        if not self.strategy:
+            print()
+            return
+
         for device in self.devices:
             if device.name.lower() == device_name.lower():
-                self.strategy.execute([device])
+                self.strategy.execute(device)
                 break
         else:
-            print(f"Device {device_name} not found")
-
-class Device:
-    def __init__(self, name):
-        self.name = name
-
-class HeatingStrategy:
-    def execute(self, devices):
-        for device in devices:
-            if "heating" in device.name.lower():
-                print(f"Turning on heating for {device.name}")
-
-class LightingStrategy:
-    def execute(self, devices):
-        for device in devices:
-            if "light" in device.name.lower():
-                print(f"Turning on lights for {device.name}")
+            print(f"Device {device_name} not found.")
 
 
-controller = HomeController()
+if __name__ == "__main__":
+    controller = HomeController()
 
+    living_room_heating = Device("Living Room Heating")
+    kitchen_light = Device("Kitchen Light")
 
-living_room_heating = Device("Living Room Heating")
-kitchen_light = Device("Kitchen Light")
+    controller.add_device(living_room_heating)
+    controller.add_device(kitchen_light)
 
-controller.add_device(living_room_heating)
-controller.add_device(kitchen_light)
+    heating_strategy = HeatingStrategy()
+    lighting_strategy = LightingStrategy()
 
-heating_strategy = HeatingStrategy()
-controller.set_strategy(heating_strategy)
-
-lighting_strategy = LightingStrategy()
-controller.set_strategy(lighting_strategy)
-
-
-user_device = input("Enter the device you want to control: ")
-if "heating" in user_device.lower():
     controller.set_strategy(heating_strategy)
-elif "light" in user_device.lower():
     controller.set_strategy(lighting_strategy)
 
-controller.control_device(user_device)
+    user_device = input("Enter the device you want to control: ")
+    controller.control_device(user_device)
