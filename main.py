8e6d3cb3928a2d48df2728742c58195fc9b2948b
AsselK2023 <122744321+AsselK2023@users.noreply.github.com>
2023-10-22 11:08:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,64 +4,98 @@ class Device:
         self.name = name
         self.state = "off"
 
-    def turn_on(self):
+    def turn_on(self, color=None):
         self.state = "on"
+        if color:
+            print(f"Turning on {self.name} with color {color}")
+        else:
+            print(f"Turning on {self.name}")
 
     def turn_off(self):
         self.state = "off"
+        print(f"Turning off {self.name}")
+
+
 
 # strategy.py
 from abc import ABC, abstractmethod
 
 class Strategy(ABC):
     @abstractmethod
-    def execute(self, device):
+    def execute(self, device, color=None):
         pass
 
 class HeatingStrategy(Strategy):
-    def execute(self, device):
+    def execute(self, device, color=None):
         if "heating" in device.name.lower():
-            device.turn_on()
+            device.turn_on(color)
             print(f"Turning on heating for {device.name}")
 
 class LightingStrategy(Strategy):
-    def execute(self, device):
+    def execute(self, device, color=None):
         if "light" in device.name.lower():
-            device.turn_on()
+            device.turn_on(color)
             print(f"Turning on lights for {device.name}")
+            
+# adapter.py
+class StrategyAdapter(Strategy):
+    def __init__(self, decorator):
+        self.decorator = decorator
+
+    def execute(self, device, color=None):
+        return self.decorator.control_device(device.name, color)
+
+
+# decorator.py
+class ColorSelectorDecorator:
+    def __init__(self, controller):
+        self.controller = controller
+
+    def control_device(self, device_name, color=None):
+        for device in self.controller.devices:
+            if device.name.lower() == device_name.lower():
+                if "light" in device.name.lower() and not color:
+                    color = input(f"Enter the color for the {device.name}: ")
+                result = self.controller.control_device(device_name, color)
+                return result
+        print(f"Device {device_name} not found.")
+
+
 
 # controller.py
 from typing import List
 
-# decorator
-def require_strategy(func):
-    def wrapper(controller, device_name):
-        if not controller.strategy:
-            print("No strategy set. Please set a strategy first.")
-        else:
-            func(controller, device_name)
-    return wrapper
-
 class HomeController:
     def __init__(self):
         self.devices: List[Device] = []
-        self.strategy: Strategy = None
+        self.strategies = {}
 
     def add_device(self, device: Device):
         self.devices.append(device)
 
-    def set_strategy(self, strategy: Strategy):
-        self.strategy = strategy
+    def set_strategy(self, device_name, strategy):
+        self.strategies[device_name.lower()] = strategy
 
-    @require_strategy  
-    def control_device(self, device_name: str):
+    def control_device(self, device_name, color=None):
+        device_name_lower = device_name.lower()
         for device in self.devices:
-            if device.name.lower() == device_name.lower():
-                self.strategy.execute(device)
+            if device.name.lower() == device_name_lower:
+                if device_name_lower in self.strategies:
+                    strategy = self.strategies[device_name_lower]
+                else:
+                    strategy = self.default_strategy(device)
+                strategy.execute(device, color)
                 break
         else:
             print(f"Device {device_name} not found.")
 
+    def default_strategy(self, device):
+        if "light" in device.name.lower():
+            return lighting_strategy
+        elif "heating" in device.name.lower():
+            return heating_strategy
+
+# Usage:
 if __name__ == "__main__":
     controller = HomeController()
 
@@ -74,8 +108,6 @@ if __name__ == "__main__":
     heating_strategy = HeatingStrategy()
     lighting_strategy = LightingStrategy()
 
-    controller.set_strategy(heating_strategy)
-    controller.set_strategy(lighting_strategy)
-
     user_device = input("Enter the device you want to control: ")
-    controller.control_device(user_device)
+    decorator = ColorSelectorDecorator(controller)
+    decorator.control_device(user_device)
